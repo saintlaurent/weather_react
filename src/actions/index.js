69de803c79fca484c7bfbc38643520bfbac0c761
Catherine Li <catherine.l@easymarkit.com>
2018-10-24 10:49:16 -0700

    Axios ajax request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
-const API_KEY = '24e88f64f98230c8f8b538eb27dfea0b'
+import axios from 'axios';
+const API_KEY = '24e88f64f98230c8f8b538eb27dfea0b';
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},us`;
+
+    //Does an ajax request
+    const request = axios.get(url);
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
   </Provider>
   , document.querySelector('.container'));
+
+  /**
+   * Action Creator describes the action created by returning the type of action and payload data associated with the action
+   * Middleware lets action pass, manipulates it, logs it or stops it
+   * Reducers return the new state
+   * Application state is changed through Redux
+   */
